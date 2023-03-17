01854cb7fc5f1669b70941a2a51f1f6d20330adf
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2023-03-17 10:13:29 +0100

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,5 +1,9 @@
 import luminance as lu
 
+#Remember to place pictures of the form 'IMG_xxx' in your directory 
+#The function 'analyze_all()' will only work for continuos naming of the pictures. For example IMG_0001, IMG_0003 ... will not work because there is IMG_0002 missing
+#You can use 'check_for_missing()' to make sure there are no pictures missing
+
 print(lu.num_all_pictures('jpg'))
 print(lu.find_first_picture('jpg')) 
 print(lu.find_last_picture('jpg'))
